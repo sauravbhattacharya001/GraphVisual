307b2fc1cdc4381e5cb8250e125d972a89e24afa
Saurav Bhattacharya <online.saurav@gmail.com>
2026-02-13 21:33:30 -0800

    Fix SQL injection and resource leaks in findMeetings.java (closes #4)
    - Replace string-concatenated SQL queries with PreparedStatement
      parameter binding in both addMeeting() and main()
    - Wrap JDBC Connection and PreparedStatement in try-with-resources
      to prevent resource leaks on exceptions
    - Reuse a single Connection for meeting insertions instead of
      opening/closing one per addMeeting() call
    - Change ResultSet concurrency to CONCUR_READ_ONLY for SELECT
      (was unnecessarily CONCUR_UPDATABLE)

--- Gvisual/src/app/findMeetings.java
+++ Gvisual/src/app/findMeetings.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,7 +45,7 @@ public class findMeetings {
         return result;
     }
 
-    public static void addMeeting(String devicePair, String startTime, String endTime, String month, String date) throws Exception {
+    public static void addMeeting(Connection conn, String devicePair, String startTime, String endTime, String month, String date) throws Exception {
         String[] imeiArr = devicePair.split("#");
         String imei1, imei2;
         if (imeiArr[0].compareTo(imeiArr[1]) > 0) {
@@ -54,163 +55,119 @@ public class findMeetings {
             imei2 = imeiArr[1];
             imei1 = imeiArr[0];
         }
-        int ap = 0;
-        String query;
-        /*Connection azialaConn = Util.getAzialaConnection();
-        java.sql.Statement azialaStmt = azialaConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-
-        String startTimeStamp = getTimeStamp(month, date, startTime);
-        String endTimeStamp = getTimeStamp(month, date, endTime);
-
-        query = "select * from (select DISTINCT ap,ssi from event as a,trace as b where a.trace = b.id and imei = '" + imei1 + "' and timestamp >= '" + startTimeStamp + "' and timestamp <= '" + endTimeStamp
-        + "' intersect "
-        + "select DISTINCT ap,ssi from event as a,trace as b where a.trace = b.id and imei = '" + imei2 + "' and timestamp >= '" + startTimeStamp + "' and timestamp <= '" + endTimeStamp+"') as d order by ssi desc limit 1";
-
-        System.out.println(query);
-        ResultSet rs = azialaStmt.executeQuery(query);
-
-        if(rs.next()){
-        ap = rs.getInt("ap");
-        }*/
         String apType = "";
 
+        String sql = "INSERT INTO meeting (imei1, imei2, starttime, endtime, location, month, date, duration) "
+                   + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-        /*switch (ap) {
-            case 7:
-            case 16:
-            case 20:
-            case 35:
-            case 38:
-            case 39:
-                apType = "public";
-                break;
-            case 29:
-            case 30:
-            case 31:
-            case 32:
-            case 33:
-            case 34:
-            case 36:
-                apType = "class";
-                break;
-            default: // all else
-                apType = "path";
-                break;
-        }*/
-
-
-
-        Connection appConn = Util.getAppConnection();
-        java.sql.Statement appStmt = appConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-
-        query = "insert into meeting (imei1,imei2,starttime,endtime,location,month,date,duration) values('" + imei1 + "','" + imei2 + "','" + startTime + "','" + endTime + "','" + apType + "','" + month + "','" + date + "'," + (int) getTimeDifference(endTime, startTime) + ")";
-        appStmt.executeUpdate(query);
-
-        appStmt.close();
-        appConn.close();
-        //azialaStmt.close();
-        //azialaConn.close();
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, imei1);
+            pstmt.setString(2, imei2);
+            pstmt.setString(3, startTime);
+            pstmt.setString(4, endTime);
+            pstmt.setString(5, apType);
+            pstmt.setString(6, month);
+            pstmt.setString(7, date);
+            pstmt.setInt(8, (int) getTimeDifference(endTime, startTime));
+            pstmt.executeUpdate();
+        }
     }
 
     public static void main(String[] args) throws Exception {
         System.out.println("connecting...");
 
-        Connection appConn = Util.getAppConnection();
-        java.sql.Statement appStmt = appConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-
-
-
-
-        ResultSet rs;
-
         int Thres = -60;
         int monthInt = 3;
         int dateInt;
 
-        for (; monthInt <= 5; monthInt++) {
-            for (dateInt = 1; dateInt <= 31; dateInt++) {
+        String selectSql = "SELECT DISTINCT rcvrimei, sndrimei, time FROM event_3 "
+                         + "WHERE month = ? AND date = ? AND sndrimei != '' AND rssi >= ?";
 
-                String month = Integer.toString(monthInt);
-                month = "0" + month;
-                String date = Integer.toString(dateInt);
-                if (dateInt < 10) {
-                    date = "0" + date;
-                }
+        try (Connection appConn = Util.getAppConnection();
+             PreparedStatement selectStmt = appConn.prepareStatement(
+                     selectSql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
 
+            for (; monthInt <= 5; monthInt++) {
+                for (dateInt = 1; dateInt <= 31; dateInt++) {
 
-                String query = "SELECT DISTINCT rcvrimei, sndrimei, time  FROM event_3 WHERE month = '" + month + "' AND date = '"
-                        + date + "' AND sndrimei != '' AND rssi >= " + Thres;
-                //System.out.println(query);
-                rs = appStmt.executeQuery(query);
-
-
-                Map<String, SortedSet<String>> deviceInteraction = new HashMap<String, SortedSet<String>>();
-
-                rs.last();
-                System.out.println("fetched " + rs.getRow() + " number of entries....still working...");
-                rs.first();
-                while (rs.next()) {
-                    if (rs.getRow() % 1000 == 0) {
-                        System.out.println("added " + rs.getRow() + " number of entries to map");
+                    String month = Integer.toString(monthInt);
+                    month = "0" + month;
+                    String date = Integer.toString(dateInt);
+                    if (dateInt < 10) {
+                        date = "0" + date;
                     }
 
-                    String imei1, imei2;
-                    if (rs.getString(1).compareTo(rs.getString(2)) > 0) {
-                        imei1 = rs.getString(2);
-                        imei2 = rs.getString(1);
-                    } else {
-                        imei1 = rs.getString(1);
-                        imei2 = rs.getString(2);
-                    }
+                    selectStmt.setString(1, month);
+                    selectStmt.setString(2, date);
+                    selectStmt.setInt(3, Thres);
 
-                    String curTime = rs.getString(3);
-                    if (curTime.length() == 11) {
-                        curTime = "0" + curTime;
-                    }
+                    ResultSet rs = selectStmt.executeQuery();
 
-                    String devicePair = imei1 + "#" + imei2;
+                    Map<String, SortedSet<String>> deviceInteraction = new HashMap<String, SortedSet<String>>();
 
-                    if (deviceInteraction.containsKey(devicePair)) {
-                        SortedSet<String> newList = deviceInteraction.get(devicePair);
-                        newList.add(curTime);
+                    rs.last();
+                    System.out.println("fetched " + rs.getRow() + " number of entries....still working...");
+                    rs.first();
+                    while (rs.next()) {
+                        if (rs.getRow() % 1000 == 0) {
+                            System.out.println("added " + rs.getRow() + " number of entries to map");
+                        }
 
-                        deviceInteraction.remove(devicePair);
-                        deviceInteraction.put(devicePair, newList);
-                    } else {
-                        SortedSet<String> newList = new TreeSet<String>();
-                        newList.add(curTime);
-                        deviceInteraction.put(devicePair, newList);
-                    }
-                }
-
-                System.out.println("Hash Map created");
-
-                System.out.println("Total different pairs detected = " + deviceInteraction.size());
-
-                for (String x : deviceInteraction.keySet()) {
-                    SortedSet<String> curSet = deviceInteraction.get(x);
-
-                    String meetingStartTime = null;
-                    String lastTime = null;
-
-                    for (String y : curSet) {
-                        if (lastTime == null) {
-                            lastTime = y;
-                            meetingStartTime = y;
-                        } else if (getTimeDifference(y, lastTime) > WINDOW_SIZE) {
-                            addMeeting(x, meetingStartTime, lastTime, month, date);
-                            System.out.println("found a meeting and inserted successfully");
-                            lastTime = y;
-                            meetingStartTime = y;
+                        String imei1, imei2;
+                        if (rs.getString(1).compareTo(rs.getString(2)) > 0) {
+                            imei1 = rs.getString(2);
+                            imei2 = rs.getString(1);
                         } else {
-                            lastTime = y;
+                            imei1 = rs.getString(1);
+                            imei2 = rs.getString(2);
+                        }
+
+                        String curTime = rs.getString(3);
+                        if (curTime.length() == 11) {
+                            curTime = "0" + curTime;
+                        }
+
+                        String devicePair = imei1 + "#" + imei2;
+
+                        if (deviceInteraction.containsKey(devicePair)) {
+                            SortedSet<String> newList = deviceInteraction.get(devicePair);
+                            newList.add(curTime);
+
+                            deviceInteraction.remove(devicePair);
+                            deviceInteraction.put(devicePair, newList);
+                        } else {
+                            SortedSet<String> newList = new TreeSet<String>();
+                            newList.add(curTime);
+                            deviceInteraction.put(devicePair, newList);
+                        }
+                    }
+
+                    System.out.println("Hash Map created");
+
+                    System.out.println("Total different pairs detected = " + deviceInteraction.size());
+
+                    for (String x : deviceInteraction.keySet()) {
+                        SortedSet<String> curSet = deviceInteraction.get(x);
+
+                        String meetingStartTime = null;
+                        String lastTime = null;
+
+                        for (String y : curSet) {
+                            if (lastTime == null) {
+                                lastTime = y;
+                                meetingStartTime = y;
+                            } else if (getTimeDifference(y, lastTime) > WINDOW_SIZE) {
+                                addMeeting(appConn, x, meetingStartTime, lastTime, month, date);
+                                System.out.println("found a meeting and inserted successfully");
+                                lastTime = y;
+                                meetingStartTime = y;
+                            } else {
+                                lastTime = y;
+                            }
                         }
                     }
                 }
             }
         }
-
-        appStmt.close();
-        appConn.close();
     }
 }
